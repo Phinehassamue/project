015d2bf958e806ce47c5d4610a4c48903893da0b
phinehas <phinehas.s@licet.ac.in>
2025-08-26 15:23:32 +0530

    first commit

---
+++ empleavesys/src/main/java/com/empleave/empleavesys/EmpleavesysApplication.java
@@ -0,0 +1,13 @@
+package com.empleave.empleavesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleavesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleavesysApplication.class, args);
+	}
+
+}
---
+++ empleavesys/src/test/java/com/empleave/empleavesys/EmpleavesysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empleave.empleavesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleavesysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
